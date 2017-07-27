48e1c37094d7e6066ff9e0360312fd4985816781
harandian <hharandian@gmail.com>
2017-07-27 11:58:17 -0700

    costum ini created

---
+++ PatiendAndDoctor/Patient.h
@@ -0,0 +1,13 @@
+//
+//  Patient.h
+//  PatiendAndDoctor
+//
+//  Created by Hirad on 2017-07-27.
+//  Copyright © 2017 Hirad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Patient : NSObject
+
+@end
---
+++ PatiendAndDoctor/Patient.m
@@ -0,0 +1,13 @@
+//
+//  Patient.m
+//  PatiendAndDoctor
+//
+//  Created by Hirad on 2017-07-27.
+//  Copyright © 2017 Hirad. All rights reserved.
+//
+
+#import "Patient.h"
+
+@implementation Patient
+
+@end
