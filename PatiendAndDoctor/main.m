294724330d8d08e1a93a20db101bb5760a6bec52
harandian <hharandian@gmail.com>
2017-07-27 18:07:41 -0700

    starting to add perscription method

--- PatiendAndDoctor/main.m
+++ PatiendAndDoctor/main.m
@@ -12,11 +12,19 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
+       
         Doctor *doctor1 = [[Doctor alloc] initWithNameDoctor: @"John Doe" doctorSpecialization: @"Dentist"];
+        NSLog(@"Hello doctor %@, I really need to see a %@ today!", doctor1.doctorName, doctor1.doctorType);
+       
+        NSLog(@"Hello!\n glad to see you came in\n what is your name and age?");
         Patient *patient1 = [[Patient alloc] initWithPatientName:@"Jane" withPatientAge:99 hasHealthCard: YES];
+        NSLog(@"My name is %@ and I am %ld years old", patient1.patientName, (long)patient1.patientAge);
+        
+        Patient *patient2 = [[Patient alloc] initWithPatientName:@"HH" withPatientAge:33 hasHealthCard: NO];
+
         
         [patient1 visitDoctor: doctor1];
+        [patient2 visitDoctor: doctor1];
         
         NSLog(@"%@",[patient1  requestMedication:doctor1]);
     
